24c9280a784a09b3d68f5329574a9c675507b8b0
Clebson Rosendo <clebsondev@gmail.com>
2023-02-05 22:17:20 -0300

    Adicionando arquivos app e html

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/teste")
+
+@app.route("/")
 def index():
-	return 'Olá Mundo!'
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
