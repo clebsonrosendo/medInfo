1c7fb809662b5e50236f9020c2f427891bc598a3
Clebson Rosendo <clebsondev@gmail.com>
2023-02-10 19:31:02 -0300

    Validação do tamanho arquivo

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,26 +1,34 @@
+let file
+let dateFormatBr
+let fileSize
+
 function uploadFile() {
-    let file = $('#input-select-file').val();
-    if (file === undefined) {
-        $('#file-details').html(
-            "<p><em>Nenhum arquivo selecionado</em></p>")
+
+    let file = document.getElementById('input-select-file').files[0];
+    let dateFormatBr = new Intl.DateTimeFormat('pt-BR').format(file.lastModifiedDate);
+    let fileSize = (file.size / 1048576).toFixed(2) + ' MB';
+
+    if (file !== '' && file.size < 5242880 ) {
+
+        $('#file-details').addClass('text-start')
+
+        $('#file-details').html('')
+        $('#file-details').append('<div><p class="fs-6">Nome: '+file.name+'</p></div>')
+        $('#file-details').append('<div><p class="fs-6">Última modificação: '+dateFormatBr+'</p></div>')
+        $('#file-details').append('<div><p class="fs-6">Tamanho: '+fileSize+'</p></div>')
+
+        $('#box-up-file').removeClass('border-light-subtle border-danger')
+        $('#box-up-file').addClass('border-success')
+
     } else {
-        console.log(file.files.length)
+
+        $('#file-details').removeClass('text-start')
+
+        $('#file-details').html('')
+        $('#file-details').append('<p class="fs-6 text-danger"><em>Arquivo com tamanho acima do permitido</em></p>')
+
+        $('#box-up-file').removeClass('border-light-subtle border-success')
+        $('#box-up-file').addClass('border-danger')
+
     }
 }
-
-function uploadFileDetails() {
-
-}
-
-
-//                        <p class="fs-6 text-secondary"><em>Nenhum arquivo selecionado</em></p>
-//                        <div><p class="fs-bold">Nome:</p></div>
-//                        <div></div>
-//                    </div>
-//                    <div>
-//                        <div><p class="fs-bold">Tamanho:</p></div>
-//                        <div></div>
-//                    </div>
-//                    <div>
-//                        <div><p class="fs-bold">Último modificação:</p></div>
-//                        <div></div>
